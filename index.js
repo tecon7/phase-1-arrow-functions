9120191dfe0e3cabdb28eafdc2cb6c91a45b840d
Teddy Economou <teddyecon@gmail.com>
2023-01-10 16:31:37 -0500

    lab complete

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => (parameter1 / parameter2)
+
+const square = (parameter1) => (parameter1 * parameter1)
+
+const add = (parameter1, parameter2) => (parameter1 + parameter2)
+    
